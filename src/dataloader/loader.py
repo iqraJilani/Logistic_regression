970f86456f36c4b2725503fdaa02d26d2cc8f01a
iqraJilani <i.jilani95@gmail.com>
2022-05-30 10:56:57 +0500

    fixed dimension issues and removing target column from data

--- src/dataloader/loader.py
+++ src/dataloader/loader.py
@@ -2,18 +2,23 @@ from .pre_processing import PreProcess
 import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
-
+import numpy as np
 
 class Loader:
     def __init__(self, path, file_name, target):
+        self.target = None
         self.data = pd.read_csv(os.path.join(path, file_name))
-        self.target = self.data[target]
+        print("original data shape: ", self.data.shape)
+        self.target = self.data.loc[:, target]
+        self.target = np.array([self.target]).reshape(-1, 1)
+        self.data = self.data.loc[:, self.data.columns != target]
+        print("X shape: ", self.data.shape)
+        print("Y shape: ", self.target.shape)
 
     def pre_process(self, methods):
         pre_obj = PreProcess(self.data)
         for transformation, cols in methods.items():
             pre_obj.fit_transform(transformation, cols)
-
         self.data = pre_obj.data
 
     def split_data(self, split_method):
--- src/pipeline/logistic_regression.py
+++ src/pipeline/logistic_regression.py
@@ -13,16 +13,17 @@ class RegressionPipeline:
         self.y_train = None
         self.X_test = None
         self.X_train = None
+        self.learning_rate = None
         self.model_obj = BuildModel()
 
     def pre_processing(self, transformations):
         loader = Loader(self.path, self.name, self.target)
         loader.pre_process(transformations)
         self.X_train, self.X_test, self.y_train, self.y_test = loader.split_data("default")
-        print(self.X_train.shape)
 
     def train_model(self, learning_rate, num_iter):
-        self.model_obj.set_data(learning_rate, self.X_train, self.y_train)
+        self.learning_rate = learning_rate
+        self.model_obj.set_data(self.learning_rate, self.X_train, self.y_train)
         for i in range(0, num_iter):
             cost_train = self.model_obj.predict().compute_cost()
             self.model_obj.compute_grad().update_params()
@@ -30,5 +31,5 @@ class RegressionPipeline:
 
     def evaluate_model(self):
         self.model_obj.set_data(self.learning_rate, self.X_test, self.y_test)
-        cost_test = self.model_obj.predict.compute_cost()
+        cost_test = self.model_obj.predict().compute_cost()
         return cost_test
