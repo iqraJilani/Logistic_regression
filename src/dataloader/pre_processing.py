7f6b84f172f7ef8c65eff156e40e13031f5d0db3
iqraJilani <i.jilani95@gmail.com>
2022-06-06 10:38:14 +0500

    fixed pre-processing for numeric columns processing and for label encoding

--- src/dataloader/pre_processing.py
+++ src/dataloader/pre_processing.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
@@ -13,30 +12,24 @@ class PreProcess:
     def __init__(self, data):
         self.data = data
         self.task = None
+        self.num_cols = None
+        self.cat_cols = None
         self.transformers = {
-            "null_values": [self.fix_null],
-            "skew": [self.fix_skew, PowerTransformer(method='box-cox')],
-            "label_encoder": [self.label_encode, LabelEncoder()],
-            "one_hot_encoder": [self.one_hot_encode, OneHotEncoder()],
-            "min_max_scaler": [self.min_max_scaler, MinMaxScaler()],
-            "standard_scaler": [self.standard_scaler, StandardScaler()],
+            "null_values": self.fix_null,
+            "skew": PowerTransformer(method='box-cox'),
+            "label_encoder": list(),
+            "one_hot_encoder": OneHotEncoder(),
+            "min_max_scaler": MinMaxScaler(),
+            "standard_scaler": StandardScaler()
         }
 
-    def fix_skew(self, target, cols):
-        """
-        fix data skewness
+    def save_encoders(self):
+        with open(os.path.join(os.getcwd(), 'data/transformers.pkl'), 'wb') as f:
+            pickle.dump(self.transformers, f)
 
-        Args:
-            target (string): name of column (target to predict) to drop whilst evaluating skewness
-            cols (list): list of columns to apply the transformation to
-        """
-        skew_feats = self.data.drop(target, axis=1).skew().sort_values(ascending=False)
-        skewness = pd.DataFrame({'Skew': skew_feats})
-        skewness = skewness[abs(skewness) > 0.5].dropna()
-        skewed_columns = skewness.index
-
-        self.transformers["skew"][1].fit(self.data[skewed_columns])
-        self.data[skewed_columns] = self.transformers["skew"][1].transform(self.data[skewed_columns])
+    def load_encoders(self):
+        with open(os.path.join(os.getcwd(), 'data/transformers.pkl'), 'rb') as f:
+            self.transformers = pickle.load(f)
 
     def fix_null(self):
         """
@@ -45,83 +38,79 @@ class PreProcess:
         """
         pass
 
-    def min_max_scaler(self, cols):
+    def fix_skew(self):
         """
-        Scale the data in range of 0 to 1
+        fix data skewness
 
         Args:
+            target (string): name of column (target to predict) to drop whilst evaluating skewness
             cols (list): list of columns to apply the transformation to
         """
+        skew_feats = self.data.skew().sort_values(ascending=False)
+        skewness = pd.DataFrame({'Skew': skew_feats})
+        skewness = skewness[abs(skewness) > 0.4].dropna()
+        skewed_columns = skewness.index
 
-        if self.task == "fit":
-            self.transformers["min_max_scaler"][1].fit(self.data[cols].values)
-        elif self.task == "transform":
-            scaled_cols = self.transformers["min_max_scaler"].transform(self.data[cols].values)
-            self.data[cols] = scaled_cols
-        elif self.task == "fit_transform":
-            self.transformers["min_max_scaler"][1].fit(self.data[cols].values)
-            scaled_cols = self.transformers["min_max_scaler"].transform(self.data[cols].values)
-            self.data[cols] = scaled_cols
+        self.transformers["skew"].fit(self.data[skewed_columns])
+        self.data[skewed_columns] = self.transformers["skew"].transform(self.data[skewed_columns])
 
+    def process_num_cols(self, scaling, task):
+        num_cols = self.data.select_dtypes(exclude=['object', 'datetime64']).columns
 
-    def standard_scaler(self, cols):
-        """
-        Standardize the numeric data to have zero mean and standard deviation of 1
-
-        Args:
-            cols (): list of columns to apply the transformation to
-        """
-
-        if self.task == "fit":
-            self.transformers["standard_scaler"][1].fit(self.data[cols].values)
-        elif self.task == "transform":
-            scaled_cols = self.transformers["standard_scaler"].transform(self.data[cols].values)
-            self.data[cols] = scaled_cols
-        elif self.task == "fit_transform":
-            self.transformers["standard_scaler"][1].fit(self.data[cols].values)
-            scaled_cols = self.transformers["standard_scaler"].transform(self.data[cols].values)
-            self.data[cols] = scaled_cols
-
-    def label_encode(self, cols):
-        """
-        Apply label encoding to categorical data
-
-        Args:
-            cols (): list of columns to apply the transformation to
-        """
-        if self.task == "fit":
-           self.data[cols] = self.data[cols].apply(lambda col: self.transformers["label_encoder"].fit_transform(cols))
-
-
-    def one_hot_encode(self, cols):
-        """
-        Apply one hot encoding to categorical data
-
-        Args:
-            cols (): list of columns to apply the transformation to
-        """
-        self.data[cols] = self.data[cols].apply(lambda col: self.transformers["one_hot_encoder"].fit_transform(col))
-        #self.data = pd.get_dummies(self.data, columns=cols)
-
-    def fit_transform(self, transformations, task):
-        """
-        function to fit the transformers to training data and transform training and test data
-
-        Args:
-            transformation (dict): transformations to apply to given list of columns
-            cols (list): list of columns to apply transformations to
-        """
-        self.task = task
+        if len(num_cols) <1:
+            return
         if task == "fit" or "fit_transform":
-            for transformation, cols in transformations.items():
-                self.transformers[transformation][0](cols)
-            with open(os.path.join(os.getcwd(), 'data/transformers.pkl'), 'wb') as f:
-                pickle.dump(self.transformers, f)
-
+            scaler = self.transformers[scaling]
+            scaler.fit(self.data[num_cols])
+            self.data[num_cols] = scaler.transform(self.data[num_cols])
+            self.save_encoders()
         elif task == "transform":
-            with open(os.path.join(os.getcwd(), 'data/transformers.pkl'), 'rb') as f:
-                self.transformers = pickle.load(f)
-            for transformation, cols in transformations.items():
-                self.transformers[transformation][0](cols)
+            self.load_encoders()
+            scaler = self.transformers[scaling]
+            self.data[num_cols] = scaler.transform(self.data[num_cols])
+
+    def process_cat_cols(self, task):
+        cat_cols = list(self.data.select_dtypes(include=['object']).columns)
+        print(cat_cols)
+        print(self.data[cat_cols[0]].shape)
+        n_cat_cols = len(cat_cols)
+
+        if len(cat_cols) < 1:
+            return
+
+        if task == "fit" or "fit_transform":
+            for i in range(0, n_cat_cols):
+                self.transformers['label_encoder'].append(LabelEncoder().fit(self.data[cat_cols[i]]))
+                self.data[cat_cols[i]] = self.transformers['label_encoder'][i].transform(self.data[cat_cols[i]])
+
+            # self.transformers['one_hot_encoder'].fit(self.data[cat_cols])
+            # self.data[cat_cols] = self.transformers['one_hot_encoder'].transform(self.data[cat_cols])
+
+            # scaler1 = self.transformers["label_encoder"]
+            # scaler2 = self.transformers["one_hot_encoder"]
+            #
+            # scaler1.fit(self.data[cat_cols])
+            # self.data[cat_cols] = scaler1.transform(self.data[cat_cols])
+            #
+            # scaler2.fit(self.data[cat_cols])
+            # self.data = scaler2.transform(self.data[cat_cols])
+
+            self.save_encoders()
+        elif task == "transform":
+            self.load_encoders()
+
+            for i in range(0, n_cat_cols):
+                self.data[cat_cols[i]] = self.transformers['label_encoder'][i].transform(self.data[cat_cols[i]])
+
+            # self.data[cat_cols] = self.transformers['one_hot_encoder'].transform(self.data[cat_cols])
+
+            # scaler1 = self.transformers["label_encoder"]
+            # scaler2 = self.transformers["one_hot_encoder"]
+            #
+            # self.data[cat_cols] = scaler1.transform(self.data[cat_cols])
+            # self.data = scaler2.transform(self.data[cat_cols])
+
+
+
 
 
